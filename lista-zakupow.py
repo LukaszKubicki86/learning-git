30302c314401c1c197f82ff14eff9e08ab2ecb3c
Łukasz Kubicki <lkubicki1986@gmail.com>
2022-09-28 19:40:12 +0200

    Init project

---
+++ lista-zakupow.py
@@ -0,0 +1,17 @@
+shopping_dict = {
+    "Piekarnia": ["bułka", "chleb","Pączek"],
+    "warzywniak": ["Marchew", "Seler", "Rukola"]
+}
+print ("lista zakupów")
+
+Piekarnia = f"idę do piekarni, kupuje tu następujące rzeczy: {shopping_dict['Piekarnia']}"
+print(Piekarnia)
+Warzywniak = f"Idę do Warzywniak, kupuję tu następujące rzeczy: {shopping_dict['warzywniak']}"
+print(Warzywniak)
+
+Piekarnia1 = len(shopping_dict["Piekarnia"])
+Warzywniak1=len(shopping_dict["warzywniak"])
+
+suma = Piekarnia1+Warzywniak1
+tekst = f"w sumie kupuję {suma} produktów"
+print (tekst)
